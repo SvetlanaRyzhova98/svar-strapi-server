62025e34ae308e068e94667547e57e350d8e719d
Svetlana98 <sveti.300498@gmail.com>
2024-09-01 16:13:54 +0500

    init

---
+++ src/api/filialy/controllers/filialy.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * filialy controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::filialy.filialy');
---
+++ src/api/statya/routes/statya.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * statya router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::statya.statya');
